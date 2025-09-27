f7bddc13d823482362702c9f7abe5caaebc3535f
alexander-darwiche <59787022+alexander-darwiche@users.noreply.github.com>
2025-09-27 05:43:26 -0400

    Update LandingPage.js

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -31,10 +31,10 @@ function LandingPage() {
         {/* Left column: text */}
         <div style={{ flex: 1, paddingRight: "20px" }}>
           <h1 style={{ fontSize: "3em", textShadow: "2px 2px 8px #000" }}>
-            World of Warcraft Classic Plus
+            World of Warcraft Classic Plus Survey
           </h1>
           <p style={{ fontSize: "1.2em", lineHeight: "1.5", textShadow: "1px 1px 4px #000" }}>
-            Welcome to the ultimate Classic Plus experience! Explore the world, relive the nostalgia, and join our community.
+            Welcome to the ultimate Classic Plus Survey! Explore the ideas that may help shape the Classic Plus we are all looking for!
           </p>
           <p style={{ fontSize: "1em", marginTop: "20px", textShadow: "1px 1px 4px #000" }}>
             Coming soon: an exclusive survey to shape the future of Classic Plus!
@@ -42,22 +42,29 @@ function LandingPage() {
         </div>
 
         {/* Right column: video */}
-        <div style={{ flex: 1, position: "relative", paddingBottom: "56.25%", height: 0 }}>
-          <iframe
-            title="WoW Classic Plus Video"
-            src="https://www.youtube.com/embed/MVUD2BqPpEc"
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              border: "none",
-              borderRadius: "10px",
-            }}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+        <div style={{ flex: 1 }}>
+            <div style={{
+                position: "relative",
+                width: "100%",
+                paddingBottom: "56.25%", // 16:9 aspect ratio
+                borderRadius: "10px",
+                overflow: "hidden",
+            }}>
+                <iframe
+                title="WoW Classic Plus Video"
+                src="https://www.youtube.com/embed/MVUD2BqPpEc"
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    border: "none",
+                }}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                ></iframe>
+            </div>
         </div>
       </div>
     </div>

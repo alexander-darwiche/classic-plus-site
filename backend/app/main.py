979a7fbd6e2e0a243d36718c6d0cac8695cb6b61
alexander-darwiche <59787022+alexander-darwiche@users.noreply.github.com>
2025-09-27 04:26:04 -0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 from app.routers import items
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "https://classic-plus-site-frontend.onrender.com/",  # React site URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI()
 app.include_router(items.router)
 
